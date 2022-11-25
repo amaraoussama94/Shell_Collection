765f0d8bfebfa83b3010cc0a6adec3b0f48eef84
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 16:02:49 +0100

    adding comment

--- reading_file.sh
+++ reading_file.sh
@@ -5,5 +5,5 @@ while IFS='' read -r LINE # -r to escape the \n line var that hold current line 
 do
     echo "line $COUNT : $LINE"
     ((COUNT++))
-done < "$1" #redirection from file to the screen 
+done < "$1" #use redirection so we  can use the passed file as input 
 exit 0
