1a4c2b1e642eb7d70d7214f10b99d0c4b7a67255
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 16:40:14 +0100

    adding 2nd methode

--- writing_file.sh
+++ writing_file.sh
@@ -3,4 +3,9 @@
 #1st methode overwriting the file or create it if doesn t exit 
  
 # . $1  >  TEST.txt
-source $1  >  TEST.txt
+#source $1  >  TEST.txt
+
+#1st methode appand to  the file or create it if doesn t exit 
+
+# . $1  >> TEST.txt
+source $1  >>  TEST.txt
