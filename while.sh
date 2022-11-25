fbc6635577a9537474f041264db020b761dd6e28
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 10:06:59 +0100

    adding some  comment

--- while.sh
+++ while.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash 
 
-#
+#Always init your var 
 
 COUNT=0 
 
-while [ $COUNT  -lt 10 ]
+while [ $COUNT  -lt 10 ] #less then  yo can use  -eq for  equal or -gt  for greater then
 do
     echo "COUNT = $COUNT"
     ((COUNT++)) #incrumenting the  value 
