97d4f67ff132610ff0b906ae3c126b5826ee3fea
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 17:59:30 +0100

    handiling bad data

---
+++ handling_bad_data.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+VALID=0
+
+while [ $VALID -eq 0]
+do
+    read -p " Please enter your name and age " NAME age
+    if [[( -z $NAME ) || ( -z $AGE )]]
+    then
+        echo " not enough parameters passed"
+        continue 
+    elif [[ ! $NAME =~ ^[A-Za-z]+$]]
+    then
+        echo "Non alpha charactere detected [$NAME]"
+        continue
+    elif [[! $AGE = ~ ^[0-9]+$]]
+        echo "Non digit charactere detected [$AGE]"
+        continue
+    fi
+    VALID=1
+done
+echo " name = $NAME , age = $AGE"
+exit 0
