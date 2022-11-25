3f265d20e403edc78f0b69ba47a4a3d2fb1d3569
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 09:55:21 +0100

    adding and  if  elif  prog

--- if_condition.sh
+++ if_condition.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash 
+
+#simple if  condition script 
+
 GUESS=$1
 if [ $GUESS = 50 ]
 then
     echo "hell yeah"
 fi
+
+#simple if  else condition script 
 
 NAME=$2
 if [ $NAME = oussama ]
@@ -11,4 +16,18 @@ then
     echo " hello  oussama"
 else
     echo "hello $NAME"
+fi
+
+#simple if elif condition script 
+
+SPORT=$3
+
+if [ $SPORT = football ]
+then 
+    echo "World  cup Qatar 2022"
+elif [ $SPORT = tennis ]
+then    
+    echo " Wimbildon 2022"
+else 
+    echo "lover of $SPORT"
 fi
