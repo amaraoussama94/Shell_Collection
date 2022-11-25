87bf137e3e4304f94fae6d4dbd16b736e8ce703b
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 14:19:02 +0100

    function basic  script

---
+++ function_basic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#1st methode to create function 
+
+function hello()
+{
+    echo "hello world"
+}
+
+echo "calling function hello"
+
+hello
+
+#2nd methode to create function 
+goodbye()
+{
+    echo " goodby world"
+}
+
+echo "calling function goodby"
+
+goodbye
+
+exit 0
