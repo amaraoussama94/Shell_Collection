734fed5aeae46d557eb28b8379b28b3a828993a8
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 11:25:31 +0100

    adding the  continue break logic

--- break_continue.sh
+++ break_continue.sh
@@ -4,9 +4,13 @@ NAMES=$@ # create an array
 
 for NAME in $NAMES ##lopping  through everyy element in the array
 do
-    if [$NAME = break]
+    if [ $NAME = break ]
     then
         break
+    fi
+    if [ $NAME = continue ]
+    then
+        continue
     fi
     echo "hello $NAME"
 done
