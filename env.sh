4bec30a4709b7b02470c0aa92fdcbd1ed4500e57
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 12:00:54 +0100

    adding env  script to disply some info

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash 
+
+#this script  display the  name of the  coùputer , user name and home directory
+
+echo "the usre name is $USER and the computer name is $HOSTNAME"   
+echo " the home  directory is $HOME"
