cb592bc98653e5c992cb67c69e827efe3751abc1
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 11:55:03 +0100

    adding some comment

--- environment_variables.sh
+++ environment_variables.sh
@@ -4,6 +4,13 @@ echo "the PATH is $PATH "
 echo "the terminal is $TERM"
 echo "the editor is $EDITOR " #mus rbe set  in the bash profile
 
+# other var  :
+# HOME : user home directory
+#HOSTNAME : host name of the machine
+#SHELL : shell that is being used 
+# USER : user of this session
+#TERM : type of command -lien terminal that is being used 
+
 if  [ -z $EDITOR ] #check for an empty string 
 then 
     echo " the editor is not set "
