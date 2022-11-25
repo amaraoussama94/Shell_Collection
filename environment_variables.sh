3d317b08de303ab9c6912a74fb8e307a2b2c6bd3
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 11:50:04 +0100

    adding some comment and change var

--- environment_variables.sh
+++ environment_variables.sh
@@ -2,4 +2,14 @@
 
 echo "the PATH is $PATH "
 echo "the terminal is $TERM"
-echo "the editor is $EDITOR "
+echo "the editor is $EDITOR " #mus rbe set  in the bash profile
+
+if  [ -z $EDITOR ] #check for an empty string 
+then 
+    echo " the editor is not set "
+fi
+
+#local change when the  script  terminate the var get its old  value 
+
+EDITOR=VSCode
+echo "the editor is $EDITOR " 
