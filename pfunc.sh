ef2edd79d0bf738ecbf896c157208c55017a43d1
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 15:33:32 +0100

    creating script with 2 function to get and shwo files

--- pfunc.sh
+++ pfunc.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env bash
 
+#get the first 10 file in the directory
+GetFiles()
+{
+    #use backticks one in number 7
+    FILES=`ls -1 | sort| head -10` 
+}
+
+ShowFiles()
+{
+    local COUNT=1
+    for FILE in $FILES
+    do 
+        echo "#$COUNT = $FILE"
+        ((COUNT++))
+    done
+}
+
+#call function 1
+GetFiles
+#call function 2
+ShowFiles
