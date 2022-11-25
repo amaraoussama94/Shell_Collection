a3ebcf6e7bda33e199cd413b0b3ad773bfa772da
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 08:18:23 +0100

    adding the  shebang to the script  and  some comment

--- sport.sh
+++ sport.sh
@@ -1,3 +1,10 @@
+#!/user/bin/env bash
+
+# Sport script - user parameters
+
 NAME=$1
-FAVORIT_SPORT=$2
+FAVORIT_SPORT=$2.
+
+#print the name  and sport 
+
 echo "your name is $1 and yopur favorit sport is $2 "
