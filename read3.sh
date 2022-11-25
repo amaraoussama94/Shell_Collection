b68d32f2b82867e8861d68d4848f9a0a8f645646
amaraoussama94 <wassa36@yahoo.fr>
2022-11-25 16:58:00 +0100

    read 3 challange script

---
+++ read3.sh
@@ -0,0 +1,18 @@
+# the shebang 
+#!/usr/bin/env bash
+
+# Script to read  a file then display the first 3 lies with counts
+
+COUNT=1
+
+while IFS='' read -r LINE
+do
+    echo " $COUNT : $LINE"
+    ((COUNT++))
+    if [ $COUNT -eq 4 ]
+    then
+        break
+    fi
+done < "$1"
+#echo " end of  script "
+exit 0
