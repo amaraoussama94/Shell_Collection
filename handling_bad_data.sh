6361b4a9820c28c452f1ea5acee23bdc65da79af
amaraoussama94 <wassa36@yahoo.fr>
2022-11-28 10:17:40 +0100

    adding  comment

--- handling_bad_data.sh
+++ handling_bad_data.sh
@@ -5,15 +5,15 @@ VALID=0
 while [ $VALID -eq 0 ]
 do
     read -p " Please enter your name and age: " NAME AGE
-    if [[( -z $NAME )   || (-z $AGE ) ]]
+    if [[( -z $NAME )   || (-z $AGE ) ]]# checking  for  empty var
     then
         echo " not enough parameters passed"
         continue 
-    elif [[ ! $NAME =~ ^[A-Za-z]+$ ]]
+    elif [[ ! $NAME =~ ^[A-Za-z]+$ ]]# checking  for non alpha character 
     then
         echo "Non alpha charactere detected [$NAME]"
         continue
-    elif [[ ! $AGE =~ ^[0-9]+$ ]]
+    elif [[ ! $AGE =~ ^[0-9]+$ ]]# checking for non digit 
     then
         echo "Non digit charactere detected [$AGE]"
         continue
