eab0cb30b9dd8527548b94eafcdd01b644a5c871
amaraoussama94 <wassa36@yahoo.fr>
2022-11-28 09:51:23 +0100

    version 1 without any check of input

--- Gessing_game.sh
+++ Gessing_game.sh
@@ -3,30 +3,32 @@
 COMPUTER=20
 WIN=0
 
-read -p "Guess the number its between 1 and 50 : " GUESS
+
 while [ $WIN -eq 0 ]
 do
-    if [ -z $GUESS ]
-    then 
-        echo " please  enter your Guess"
-        continue
-    fi
+    read -p "Guess the number its between 1 and 50 : " GUESS
+
+    #if [ -z $GUESS ]
+    #then 
+    #    echo " please  enter your Guess" 
+    #    continue
+    #fi
 
     if [   $GUESS -eq $COMPUTER ]
     then 
         echo "  you Guess it "
         WIN=1
+        exit 0
     elif  [  $GUESS -lt $COMPUTER ]
     then
         echo "  you did not Guess it you re too low "
         continue
     else
         echo "  you did not Guess it you re too high "
-        #continue
+        continue
     fi
 done 
 
-echo " You win the game "
 exit 0
 
 
